c0a90ebe9517a46380293127351e109f231f9a74
ShaqIbarra <shaqibarra@gmail.com>
2022-01-28 19:10:32 -0600

    ..

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,33 +1,40 @@
-
+const pool = require('../db')
 
 const getAlltask = async (req, res) => {
     res.send('Retrieving a list of task');
-}
+};
 
 const getTask = (req, res) => {
     res.send('Retrieving a single task');
-}
+};
 
-const postTask = (req, res) => {
+const postTask = async (req, res) => {
+    const {title, description} = req.body
+
+  const result = await pool.query("INSERT INTO task (title, description) VALUES ($1, $2)", [
+      title,
+      description,
+  ]);
+
     res.send('Creating a task');
-}
+};
 
 const putTask = (req, res) => {
     res.send('Updating a task');
-}
+};
 
 const deleteTask = (req, res) => {
     res.send('Deleting a task');
-}
+};
+
+ 
 
 
 
-
-
-module.exports ={ 
+module.exports = { 
     getAlltask,
     getTask,
     postTask,
     putTask,
     deleteTask
-}
+};
